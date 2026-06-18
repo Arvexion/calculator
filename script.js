56294c5500cec3b8855c709f1232a8fb8f92f171
Arvexion <288262116+Arvexion@users.noreply.github.com>
2026-06-18 12:43:51 +0800

    Add operate function that returns results

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ let firstNum = 0;
 let operator = null;
 let secondNum = 0;
 
+// Returns the result of the calculator operation 
+function operate(firstNum, operator, secondNum) {
+    switch (operator) {
+        case '+':
+            return add(firstNum, secondNum);
+        case '-':
+            return subtract(firstNum, secondNum);
+        case '*':
+            return multiply(firstNum, secondNum);
+        case '/':
+            return divide(firstNum, secondNum);
+        default: 
+            return "ERROR || Invalid Operator";
+    }
+}
 
 // Returns the sum of two numbers
 function add(a, b) {
